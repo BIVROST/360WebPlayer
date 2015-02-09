12c9b4a67138babfe1aa8de52b606a33592d2ab3
Krzysztof Bociurko <git@chanibal.pl>
2015-02-09 11:31:00 +0100

    Rendering an image on a sphere

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 Bivrost.js
 ==========
 
-[ ] przygotowanie projektu (znaleźć wideo, threejs itp)
+[x] przygotowanie projektu (znaleźć wideo, threejs itp)
+[x] wyświetlenie tekstury
 [ ] obsługa wideo jako tekstury
 [ ] wyświetlenie wideo na sferze
 [ ] mouselook
 [ ] wyświetlenie dwóch wideo na dwóch sferach każda dla osobnej kamery
 [ ] obsługa vr.js
+
+Powiązane:
+- http://stemkoski.github.io/Three.js/Video.html
+- http://threejs.org/examples/webgl_materials_video.html
+- http://diveintohtml5.info/video.html
+- https://blog.mediacru.sh/2013/12/23/The-right-way-to-encode-HTML5-video.html
--- bivrost.js
+++ bivrost.js
@@ -7,18 +7,39 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var cube=null;
 
-camera.position.z = 5;
+var loader=new THREE.TextureLoader();
+loader.load(
+	"scenes/holland.jpeg", 
+	function done(texture) {
+		scene.add(cube=new THREE.Mesh(
+			new THREE.SphereGeometry(3, 50, 20),
+			new THREE.MeshBasicMaterial({
+				map: texture,
+				overdraw: 0.5,
+				side: THREE.BackSide
+			})
+		));
+	}, 
+	function progress(xhr) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+	}
+);
 
+//camera.position.z = 5;
+
+
+var clock=new THREE.Clock();
 function render() {
-	cube.rotation.x+=0.1;
-//	cube.rotation.y+=0.05;
+	var dt=clock.getDelta();
 	
-	requestAnimationFrame( render );
+	if(cube) {
+		cube.rotation.x+=dt*.1;
+		cube.rotation.y+=dt*1;
+	}
+	
 	renderer.render( scene, camera );
+	requestAnimationFrame( render );
 }
 render();
