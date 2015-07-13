500fa0f51e30b27b85f86ad52286f9df82e70104
Krzysztof Bociurko <git@chanibal.pl>
2015-07-13 18:19:32 +0200

    Added an IE-Edge meta to mock page

--- show.php
+++ show.php
@@ -5,7 +5,8 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" type="text/css" href="style.css" />
-	</head>
+                <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+        </head>
 	<body class="show">
 		
 		<article>
