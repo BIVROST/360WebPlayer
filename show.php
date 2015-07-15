e08ceae741516e2165d2644307ea8475df28c07e
Krzysztof Bociurko <git@chanibal.pl>
2015-07-15 14:43:16 +0200

    Split css into page (style.scss/css) and widget (bivrost.scss/css)

--- show.php
+++ show.php
@@ -5,7 +5,8 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" type="text/css" href="style.css" />
-                <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+		<link rel="stylesheet" type="text/css" href="bivrost.css" />
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
         </head>
 	<body class="show">
 		
@@ -40,7 +41,7 @@
 		
 		
 		<script src="three.js" type="text/javascript"></script>
-		<script src="VRControls.js" type="text/javascript"></script>
+		<!--<script src="VRControls.js" type="text/javascript"></script>-->
 		<!--<script src="VREffect.js" type="text/javascript"></script>-->
 		<script src="OculusRiftEffect.js" type="text/javascript"></script>
 		<script src="q.js" type="text/javascript"></script>
