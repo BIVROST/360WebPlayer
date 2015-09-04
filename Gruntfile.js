62fd429be5eb6368d5ba26978e1a038e490c1bb7
Krzysztof Bociurko <git@chanibal.pl>
2015-09-04 17:50:39 +0200

    Building with Grunt and Closure Compiler

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		
+		pkg: grunt.file.readJSON("package.json"),
+		
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded',
+					compass: true,
+					sourcemap: "none",
+					trace: true
+				},
+				files: {
+					'output/bivrost.css': 'bivrost.scss',
+					'output/bivrost-white.css': 'bivrost-white.scss'
+				}
+			}
+		},
+		
+		"closure-compiler": {
+			frontend: {
+				closurePath: './compiler.jar',
+				js: grunt.file.readJSON("scripts.json"),
+				jsOutputFile: 'output/bivrost.js',
+//				maxBuffer: 5000,
+				options: {
+					compilation_level: 'SIMPLE',
+					language_in: 'ECMASCRIPT6',
+					language_out: 'ECMASCRIPT5'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-closure-compiler');
+
+	grunt.registerTask('default', ['sass', 'closure-compiler']);
+};
+
+// scss -> css
+// js | concat | closure -> js
