02fc6ad4be6c7ff98d007bd40a70f83ca525aa13
Krzysztof Bociurko <git@chanibal.pl>
2016-04-06 13:45:37 +0200

    Cleand up tag loader code

--- src/bivrost-loader.js
+++ src/bivrost-loader.js
@@ -13,30 +13,104 @@ Bivrost.Loader=function(dom) {
 	
 	
 	/**
-	 * Helper returning an custom or data attribute 
-	 * @param {HTMLEleemnt} element
-	 * @param {string} name
-	 * @param {?string} throws
-	 * @returns {string|null}
-	 */
-	function attr(element, name, throws) {
-		if(element.hasAttribute(name))
-			return element.getAttribute(name);
-		if(element.hasAttribute("data-bivrost-"+name))
-			return element.getAttribute("data-bivrost-"+name);
-		if(throws)
-			throw throws;
-		return undefined;
-	};
-	
-	/**
+	 * Asserts that the value is one of allowed
+	 * @private
 	 * @param {string} value
 	 * @param {array<string>} allowed
-	 * @param {string} throws
+	 * @param {string} throws - the message that is thrown as an exception if assertion fails
+	 * @return the value if correct
 	 */
 	function assert(value, allowed, throws) {
 		if(allowed.indexOf(value) === -1)
 			throw throws;
+		return value;
+	}
+	
+	
+	/**
+	 * Helper returning an custom or data attribute as a string
+	 * @private
+	 * @param {HTMLElement} element
+	 * @param {string} name
+	 * @param {?*|?{message:string, is_thrower:bool}} defaultValue - set value or use argumentIsRequiredHelper
+	 * @returns {string|undefined}
+	 */
+	function attrString(element, name, defaultValue) {
+		if(element.hasAttribute(name))
+			return element.getAttribute(name);
+		
+		if(element.hasAttribute("data-bivrost-"+name))
+			return element.getAttribute("data-bivrost-"+name);
+		
+		if(typeof(defaultValue) === "undefined")
+			return undefined;
+		
+		if(defaultValue.hasOwnProperty("is_thrower"))
+			throw defaultValue.message;
+		
+		return defaultValue.toString();
+	};
+
+
+	/**
+	 * Helper returning an custom or data attribute as a boolean, available values of attribute are "true" and "false"
+	 * @private
+	 * @param {HTMLElement} element
+	 * @param {string} name
+	 * @param {?*|?{message:string, is_thrower:bool}} defaultValue - set value or use argumentIsRequiredHelper, note: the default value should be in string form
+	 * @returns {boolean|undefined}
+	 */
+	function attrBool(element, name, defaultValue) {
+		var value=attrString(element, name, defaultValue);
+		assert(value, ["true", "false"], name+" must be true or false");
+		return value === "true";
+	}
+
+
+	/**
+	 * Helper returning an custom or data attribute as a number, available values of attribute are "true" and "false"
+	 * @private
+	 * @param {HTMLElement} element
+	 * @param {string} name
+	 * @param {?*|?{message:string, is_thrower:bool}} defaultValue - set value or use argumentIsRequiredHelper, note: the default value should be in string form
+	 * @returns {number|undefined}
+	 */
+	function attrNumber(element, name, defaultValue) {
+		var value=attrString(element, name, defaultValue);
+		if(!/^\d+(.\d*)?$/.test(value))
+			throw name+" is not a number";
+		return value === "true";
+	}
+	
+	
+	/**
+	 * Helper returning an custom or data attribute as a string, available values of attribute are to be sent using availableValues param
+	 * @private
+	 * @param {HTMLElement} element
+	 * @param {string} name
+	 * @param {?*|?{message:string, is_thrower:bool}} defaultValue - set value or use argumentIsRequiredHelper
+	 * @param {array<string>} availableValues
+	 * @returns {string|undefined}
+	 */
+	function attrEnum(element, name, defaultValue, availableValues) {
+		var value=attrString(element, name, defaultValue);
+		assert(value, availableValues, name+" must be "+availableValues.join(" or "));
+		return value;
+	}
+	
+	
+	/**
+	 * Helper to be used as the argument defaultValue of attr_* functions, if used there it will make
+	 * the attr_* helper throw an exception if the argument is not found
+	 * @private
+	 * @param {type} message
+	 * @returns {Bivrost.Loader.helper_targumentIsRequiredHelper-loaderAnonym$1}
+	 */
+	function argumentIsRequiredHelper(message) {
+		return {
+			is_thrower: true,
+			message: message
+		};
 	}
 	
 	
@@ -44,34 +118,30 @@ Bivrost.Loader=function(dom) {
 		var urls={};
 		
 		// root url+type configuration
-		var url=attr(container, "url");
-		var type=attr(container, "type");
+		var url=attrString(container, "url");
+		var type=attrString(container, "type");
 		if(url)
 			urls[url]=type;
 	
 		// additional media-source url+type
 		[].slice.call(container.querySelectorAll("bivrost-media, [data-bivrost-media]")).forEach(function(ee) {
-			urls[attr(ee, "url", "url (or data-bivrost-url) attribute is required in the bivrost-media tag")]=attr(ee, "type");
+			var url=attrString(ee, "url", argumentIsRequiredHelper("url (or data-bivrost-url) attribute is required in the bivrost-media tag"));
+			var type=attrString(ee, "type");
+			urls[url]=type;
 		});
 		
 		log("loading media: ", container, urls);
 
-		var stereoscopy=attr(container, "stereoscopy") || Bivrost.STEREOSCOPY_AUTODETECT;
-		assert(stereoscopy, Bivrost.AVAILABLE_STEREOSCOPIES, "stereoscopy must be "+Bivrost.AVAILABLE_STEREOSCOPIES.join(" or "));
+		var stereoscopy=attrEnum(container, "stereoscopy", Bivrost.STEREOSCOPY_AUTODETECT, Bivrost.AVAILABLE_STEREOSCOPIES);
 
-		var projection=attr(container, "projection") || Bivrost.PROJECTION_EQUIRECTANGULAR;
+		var projection=attrString(container, "projection") || Bivrost.PROJECTION_EQUIRECTANGULAR;
 		assert(projection.replace(/:.+/, ""), Bivrost.AVAILABLE_PROJECTIONS, "projection must be "+Bivrost.AVAILABLE_PROJECTIONS.join(" or "));
 		
-		var source=attr(container, "source") || Bivrost.SOURCE_AUTODETECT;
-		assert(source, Bivrost.AVAILABLE_SOURCES, "source must be "+Bivrost.AVAILABLE_SOURCES.join(" or "));
+		var source=attrEnum(container, "source", Bivrost.SOURCE_AUTODETECT, Bivrost.AVAILABLE_SOURCES);
 		
-		var loop=attr(container, "loop") || "false";
-		assert(loop, ["true", "false"], "loop must be true or false");
-		loop=loop === "true";
-		
-		var autoplay=attr(container, "autoplay") || "true";
-		assert(autoplay, ["true", "false"], "autoplay must be true or false");
-		autoplay=autoplay === "true";
+		var loop=attrBool(container, "loop", false);
+
+		var autoplay=attrBool(container, "autoplay", true);
 		
 		return new Bivrost.Player(container, urls, projection, stereoscopy, source, JSON.parse(loop), JSON.parse(autoplay));
 	});
